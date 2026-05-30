659879c78bd80744d3ea19fb419ed88a3d867ead
examforge99 <examforge99@gmail.com>
2026-05-31 00:36:52 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content="ExamForge" />
-          <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+<link rel="apple-touch-icon" href="/icon-192.png" />
         </head>
         <body>
           {children}
