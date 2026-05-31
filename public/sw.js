0bc7da2dc90966529ff60e45ad230e7482210fa7
examforge99 <examforge99@gmail.com>
2026-05-31 13:23:51 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,25 +3,32 @@ const STATIC_ASSETS = ['/', '/dashboard', '/manifest.json']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
   )
-  self.skipWaiting()
 })
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    caches.keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+        )
+      )
+      .then(() => self.clients.claim())
   )
-  self.clients.claim()
 })
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return
+
   event.respondWith(
     fetch(event.request)
       .then((res) => {
+        if (!res || res.status !== 200 || res.type === 'opaque') return res
+
         const clone = res.clone()
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
         return res
